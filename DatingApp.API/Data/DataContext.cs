c2c3c46d99604df7a4e083e03b19d7a84ebd1868
andrewboy <andrew.boy@entelect.co.za>
2019-02-22 14:21:47 +0200

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options){}
+
+        public DbSet<Value> Values {get; set; } //The name of this will be the name of the SQL table when it is created.
+    }
+}
